94e0b818afb791182ea4db7babae3a32498c0f06
Shawn Junell <Shawn.Junell@pemco.com>
2018-02-27 16:46:23 -0800

    update objmusic

--- objmusic/objmusic.py
+++ objmusic/objmusic.py
@@ -1,2 +1,19 @@
 from .tools import *
+import sys
 
+KEY = None
+SCALE = None
+PITCH_X = None
+PITCH_Y = None
+TIME_SET = None
+TIME_X = None
+TIME_Y = None
+ORDER = None
+
+def main():
+   __p = objmusic.tools.PitchSet(SCALE,KEY,PITCH_X,PITCH_Y).get_grid()
+   __t = objmusic.tools.TimeSet(TIME_SET,TIME_X,TIME_Y).get_grid()
+   # __o = obj
+
+if __name__ == '__main__':
+    main()
